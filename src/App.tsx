8540af1810ac4fdda2611f77546200f186d4814d
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2024-02-20 01:42:55 +0500

    Add bug list ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import './App.css'
 import { Button } from './components/ui/button'
 
 function App() {
-
   return (
     <div>
+      <h1 className="text-2xl mb-3">
+        Bug with using custom tailwind fontSize config in shadcn
+      </h1>
       <Button className="text-4xl">text-4xl</Button>
-      <Button className="text-h1">text-h1 (50px)</Button>
+      <Button className="text-h1">text-h1</Button>
+      <div className="text-left">
+        <h2 className="text-2xl mt-5 font-bold">Bug list</h2>
+        <ul className='list-disc'>
+          <li>"text-h1" should set fontSize to 50px</li>
+          <li>
+            Using "text-h1" should not override "text-primary-foreground" color
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
