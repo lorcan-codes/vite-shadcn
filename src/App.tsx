f71e8ecc4a718686e24cf44b9b99cc7a7bc5e651
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2024-02-20 01:00:25 +0500

    Shadcn vite starter project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+     <h1 className='text-2xl'>Hello</h1>
+     <Button>Button</Button>
+    </div>
+  )
+}
+
+export default App
